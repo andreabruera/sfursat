3c11d6865d5d5721fcf6569530fd128f52eb2a12
Andrea <andrea.bruera@gmail.com>
2023-11-21 09:37:38 +0000

    ready to measure

--- analysis.py
+++ analysis.py
@@ -3,6 +3,14 @@ import numpy
 import os
 import re
 import scipy
+import spacy
+
+from scipy import spatial
+from tqdm import tqdm
+
+from utils import curel, seqrel, switches_and_clusters
+
+spacy_model = spacy.load('de_core_news_lg')
 
 ### read dataset
 lines = list()
@@ -21,7 +29,7 @@ total_rows = l_i
 rts = {int(sub) : {
                      task : {
                          cond : {
-                                cat : list() for cat in set(full_dataset['item'])
+                                #cat : list() for cat in set(full_dataset['item'])
                                 } 
                          for cond in set(full_dataset['cond'])
                          } 
@@ -32,7 +40,7 @@ rts = {int(sub) : {
 fluencies = {int(sub) : {
                      task : {
                          cond : {
-                                cat : list() for cat in set(full_dataset['item'])
+                                #cat : list() for cat in set(full_dataset['item'])
                                 } 
                          for cond in set(full_dataset['cond'])
                          } 
@@ -47,6 +55,21 @@ for row in range(total_rows):
     cat = full_dataset['item'][row]
     rt = float(full_dataset['rt'][row])
     word = full_dataset['response'][row].strip()
+    if cat not in rts[sub][task][cond].keys():
+        rts[sub][task][cond][cat] = list()
+        fluencies[sub][task][cond][cat] = list()
     rts[sub][task][cond][cat].append(rt)
     fluencies[sub][task][cond][cat].append(word)
 import pdb; pdb.set_trace()
+
+word_vecs = dict()
+lemma_vecs = dict()
+ft = fasttext.load_model(os.path.join('..', '..', 'dataset', 'word_vectors', 'de', 'cc.de.300.bin'))
+for row in tqdm(range(total_rows)):
+    task = full_dataset['task'][row]
+    if 'sem' in task:
+        word = full_dataset['response'][row].strip()
+        lemma = ' '.join([w.lemma_ for w in spacy_model(word)]).lower()
+        word_vecs[word] = ft.get_word_vector(word)
+        lemma_vecs[word] = ft.get_word_vector(lemma)
+        #print(1-scipy.spatial.distance.cosine(word_vecs[word], lemma_vecs[word]))
---
+++ utils.py
@@ -0,0 +1,6 @@
+def curel(words, model):
+    pass
+def seqrel(words, model):
+    pass
+def switches_and_clusters(words, model):
+    pass
